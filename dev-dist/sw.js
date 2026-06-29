adc6c3d4bd75202275e8eb9cc34f7cc93c79553d
Tolaj <swapnilhgf@gmail.com>
2026-06-29 06:12:36 -0400

    add: splash screen animation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d08cb3c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.847h33v8q18"
+    "revision": "0.gtj46lltv98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
