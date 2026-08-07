b2b37003b6381ccc1d82677cbcbd563b597732cb
Tolaj <swapnilhgf@gmail.com>
2026-08-07 16:12:25 -0400

    add: implement transaction category combobox and integrate category creation in Finance component

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -69,6 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-d08cb3c2'], (function (workbox) { 'use strict';
 
+  importScripts("push-sw.js");
   self.skipWaiting();
   workbox.clientsClaim();
   /**
@@ -81,7 +82,7 @@ define(['./workbox-d08cb3c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s8jr7j3nnj8"
+    "revision": "0.abn2nbcbaio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
